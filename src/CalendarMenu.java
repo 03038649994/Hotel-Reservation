a0209b07f5b37d3db9637e7f662eaeacbede9dd1
EnigmaKardos <email_kados@yahoo.com>
2016-11-10 16:33:39 -0800

    Added the Start Menu

---
+++ src/CalendarMenu.java
@@ -0,0 +1,45 @@
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.*;
+
+
+public class CalendarMenu {
+
+	public static void main(String[] args) {
+		final int BUTTON_WIDTH=400;
+		final int BUTTON_HEIGHT=50;
+		Dimension btnDim = new Dimension(BUTTON_WIDTH, BUTTON_HEIGHT);
+		
+		JFrame frame= new JFrame();
+		frame.setSize(500, 500);
+		
+		JButton guest= new JButton("Guest");
+		guest.setPreferredSize(btnDim);
+		
+		guest.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+				GuestLogin f=new GuestLogin();
+			}
+		});
+		
+		JButton manager= new JButton("Manager");
+		manager.setPreferredSize(btnDim);
+		
+		JLabel label= new JLabel("Guest Or Manager?");
+		
+		frame.setLayout(new BorderLayout());
+		frame.add(label, BorderLayout.NORTH);
+		frame.add(guest, BorderLayout.CENTER);
+		frame.add(manager,BorderLayout.SOUTH);
+		
+		frame.setTitle("Hotel Reservation System");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.pack();
+		frame.setVisible(true);
+	}
+
+}
