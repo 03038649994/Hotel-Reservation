867537cae675c79047d3c222021917d3c6c5ce23
Karan Bhargava <karan1525@yahoo.co.in>
2016-11-27 22:17:13 -0800

    Managerial UI push

---
+++ src/HotelReservationSystem.java
@@ -0,0 +1,12 @@
+
+public class HotelReservationSystem {
+
+	public static void main (String[] args) {
+		Hotel hotel = new Hotel();
+		
+		hotel.addUser(new Manager(0, "manager", "manager"));
+		
+		//method to start the GUI
+	
+	}
+}
