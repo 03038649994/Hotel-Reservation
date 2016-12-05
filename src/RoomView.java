3e2aa21ff2b0e620980179add882c673e84eabcc
DrightenSlayer <donut-slayer@live.com>
2016-12-05 15:25:13 -0800

    Finished Manager UI (functional)

---
+++ src/RoomView.java
@@ -0,0 +1,109 @@
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Calendar;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+public class RoomView extends JPanel
+{
+	private static final long serialVersionUID = 1L;
+	public RoomView(Hotel h)
+	{
+		setMaximumSize(new Dimension(500, 200));
+		setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
+		add(new RoomPanel(h));
+		add(new RoomInfoPanel(h));
+	}
+	private static class RoomPanel extends JPanel
+	{
+		private static final long serialVersionUID = 1L;
+
+		public RoomPanel(Hotel h)
+		{
+			setMaximumSize(new Dimension(215, 200));
+			setLayout(new GridLayout(0,3,0,0));
+
+			List<Room> rooms = h.getRooms();
+			for(Room room : rooms)
+			{
+				JButton roomBtn = new JButton("Room "+room.getRoomNumber());
+				roomBtn.addActionListener(new
+						ActionListener()
+				{
+					@Override
+					public void actionPerformed(ActionEvent e)
+					{
+						h.setSelectedRoom(room);
+					}
+
+				});
+				add(roomBtn);
+			}
+		}
+	}
+	private static class RoomInfoPanel extends JPanel implements ChangeListener
+	{
+		private static final long serialVersionUID = 1L;
+		private JTextArea roomInfoText;
+		private Hotel h;
+		public RoomInfoPanel(Hotel h)
+		{
+			this.h = h;
+			h.attach(this);
+			setMaximumSize(new Dimension(285, 200));
+			roomInfoText = new JTextArea(100, 20);
+			roomInfoText.setEnabled(false);
+			stateChanged(new ChangeEvent(this));
+			add(roomInfoText);
+		}
+
+		@Override
+		public void stateChanged(ChangeEvent arg0)
+		{
+			if(h.getSelectedRoom()==null)
+			{
+				roomInfoText.setText("Please choose a room to view details.\n");
+			}
+			else
+			{
+				Iterator<Reservation> r = h.reservationIterator(h.getSelectedDate(), h.getSelectedRoom());
+				if(r.hasNext())
+				{
+					roomInfoText.setText("Room " + h.getSelectedRoom().getRoomNumber() + " Details (Confidential)\n");
+				}
+				else
+				{
+					roomInfoText.setText("There are no tennants at this time.");
+				}
+				while(r.hasNext())
+				{
+					Reservation res = r.next();
+					roomInfoText.append("Tennant name: "+h.findUserByID(res.getID()).getUserName() +"\n");
+					roomInfoText.append("Tennant ID: "+res.getID() +"\n");
+
+					Calendar start = res.getStartDate(); Calendar end = res.getEndDate();
+					int day1 = start.get(Calendar.DATE); int day2 = end.get(Calendar.DATE);
+					int month1 = start.get(Calendar.MONTH)+1; int month2 = end.get(Calendar.MONTH)+1;
+					int year1 = start.get(Calendar.YEAR); int year2 = end.get(Calendar.YEAR);
+					int duration = end.get(Calendar.DAY_OF_YEAR)-start.get(Calendar.DAY_OF_YEAR);
+					roomInfoText.append("Start date: "+month1+"/"+day1+"/"+year1+"\n");
+					roomInfoText.append("End date: "+month2+"/"+day2+"/"+year2+"\n");
+					roomInfoText.append("Duration of stay: "+duration+" "+((duration>1)? "nights" : "night")+"\n");
+					roomInfoText.append("Transaction ID: "+res.getTransactionID() +"\n");
+				}
+			}
+		}
+	}
+}
